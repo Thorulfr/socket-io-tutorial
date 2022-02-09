f9ba31d681505c7435a39ab412acb145f1520966
Ben H <deepfreespace@gmail.com>
2022-02-09 15:42:18 -0700

    Connect/disconnect messages

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+// Configure Socket.IO
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 // Configure home route
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+// Listen for incoming sockets and log them
+io.on('connection', (socket) => {
+    console.log('A user connected!');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected.');
+    });
 });
 
 // Initialize server
