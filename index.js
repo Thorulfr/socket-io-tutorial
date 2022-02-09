91dc2231183236d9079cc43985f01529c091f7df
Ben H <deepfreespace@gmail.com>
2022-02-09 13:05:14 -0700

    Indexindent using spaces

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 // Configure home route
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 // Initialize server
